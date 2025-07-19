baadef7ea6b25496743543e9f006a0aca987f0f4
Klein Petr <petr@kleinpetr.com>
2025-07-19 19:42:25 +0200

    fix(ci): lint:fix

--- src/index.ts
+++ src/index.ts
@@ -12,14 +12,29 @@ import LANGUAGES from './codes/languages'
 import * as CODES from './types'
 
 // Export arrays with codes only
-export const CURRENCY_CODES: CODES.CurrencyCode[] = CURRENCIES.map(currency => currency.code)
-export const COUNTRY_CODES_2: CODES.CountryCode2[]  = COUNTRIES.map(country => country.code2)
-export const COUNTRY_CODES_3: CODES.CountryCode3[] = COUNTRIES.map(country => country.code3)
-export const COUNTRY_CURRENCIES: CODES.CountryCurrency[] = COUNTRIES.map(country => country.currency)
-export const COUNTRY_PHONE_CODES: CODES.CountryPhoneCode[] = COUNTRIES.map(country => country.phoneCode)
-export const COUNTRY_VATS: CODES.CountryVat[] = COUNTRIES.map(country => country.vat)
-export const BANK_CODES: CODES.BankCode[] = BANKS.map(bank => bank.code)
-export const LANGUAGE_CODES: CODES.LanguageCode[] = LANGUAGES.map(language => language.code)
-export const CURRENCY_PROVIDER_IDS: CODES.CurrencyProviderId[] = CURRENCY_PROVIDERS.map(provider => provider.id)
+export const CURRENCY_CODES: CODES.CurrencyCode[] = CURRENCIES.map(
+  (currency) => currency.code,
+)
+export const COUNTRY_CODES_2: CODES.CountryCode2[] = COUNTRIES.map(
+  (country) => country.code2,
+)
+export const COUNTRY_CODES_3: CODES.CountryCode3[] = COUNTRIES.map(
+  (country) => country.code3,
+)
+export const COUNTRY_CURRENCIES: CODES.CountryCurrency[] = COUNTRIES.map(
+  (country) => country.currency,
+)
+export const COUNTRY_PHONE_CODES: CODES.CountryPhoneCode[] = COUNTRIES.map(
+  (country) => country.phoneCode,
+)
+export const COUNTRY_VATS: CODES.CountryVat[] = COUNTRIES.map(
+  (country) => country.vat,
+)
+export const BANK_CODES: CODES.BankCode[] = BANKS.map((bank) => bank.code)
+export const LANGUAGE_CODES: CODES.LanguageCode[] = LANGUAGES.map(
+  (language) => language.code,
+)
+export const CURRENCY_PROVIDER_IDS: CODES.CurrencyProviderId[] =
+  CURRENCY_PROVIDERS.map((provider) => provider.id)
 
 export { CODES }
