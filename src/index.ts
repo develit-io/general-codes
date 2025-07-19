03e2bb6346784ee53b459b972773eb2c71ac0e66
Klein Petr <petr@kleinpetr.com>
2025-07-19 21:52:54 +0200

    feat: export arrays as readonly consts to be used in zod

--- src/index.ts
+++ src/index.ts
@@ -11,33 +11,39 @@ import CURRENCY_PROVIDERS from './codes/currency-providers'
 import LANGUAGES from './codes/languages'
 import * as CODES from './types'
 
-// Export arrays with codes only
-export const CURRENCY_CODES = CURRENCIES.map(
-  (currency) => currency.code,
-) satisfies readonly CODES.CurrencyCode[]
-export const COUNTRY_CODES_2 = COUNTRIES.map(
-  (country) => country.code2,
-) satisfies readonly CODES.CountryCode2[]
-export const COUNTRY_CODES_3 = COUNTRIES.map(
-  (country) => country.code3,
-) satisfies readonly CODES.CountryCode3[]
-export const COUNTRY_CURRENCIES = COUNTRIES.map(
-  (country) => country.currency,
-) satisfies readonly CODES.CountryCurrency[]
-export const COUNTRY_PHONE_CODES = COUNTRIES.map(
-  (country) => country.phoneCode,
-) satisfies readonly CODES.CountryPhoneCode[]
-export const COUNTRY_VATS = COUNTRIES.map(
-  (country) => country.vat,
-) satisfies readonly CODES.CountryVat[]
-export const BANK_CODES = BANKS.map(
-  (bank) => bank.code,
-) satisfies readonly CODES.BankCode[]
-export const LANGUAGE_CODES = LANGUAGES.map(
-  (language) => language.code,
-) satisfies readonly CODES.LanguageCode[]
-export const CURRENCY_PROVIDER_IDS = CURRENCY_PROVIDERS.map(
-  (provider) => provider.id,
-) satisfies readonly CODES.CurrencyProviderId[]
+// Export arrays with codes only as readonly tuples with literal types
+export const CURRENCY_CODES = [
+  ...CURRENCIES.map((currency) => currency.code),
+] as const
+
+export const COUNTRY_CODES_2 = [
+  ...COUNTRIES.map((country) => country.code2),
+] as const
+
+export const COUNTRY_CODES_3 = [
+  ...COUNTRIES.map((country) => country.code3),
+] as const
+
+export const COUNTRY_CURRENCIES = [
+  ...COUNTRIES.map((country) => country.currency),
+] as const
+
+export const COUNTRY_PHONE_CODES = [
+  ...COUNTRIES.map((country) => country.phoneCode),
+] as const
+
+export const COUNTRY_VATS = [
+  ...COUNTRIES.map((country) => country.vat),
+] as const
+
+export const BANK_CODES = [...BANKS.map((bank) => bank.code)] as const
+
+export const LANGUAGE_CODES = [
+  ...LANGUAGES.map((language) => language.code),
+] as const
+
+export const CURRENCY_PROVIDER_IDS = [
+  ...CURRENCY_PROVIDERS.map((provider) => provider.id),
+] as const
 
 export { CODES }
