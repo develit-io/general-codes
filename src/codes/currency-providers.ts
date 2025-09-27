c01ec73c81eb1b1015c7e0fb91e3ed91755d54a4
Klein Petr <petr@kleinpetr.com>
2025-09-27 21:41:58 +0200

    chore: lint fix

--- src/codes/currency-providers.ts
+++ src/codes/currency-providers.ts
@@ -19,11 +19,13 @@ const currencyProviders = [
   },
   { id: 'CNB', url: 'https://api.cnb.cz/cnbapi/exrates/daily' },
   {
-    id: 'tradermade-live', url: 'https://marketdata.tradermade.com/api/v1/live?currency={pairs}&api_key={access_key}',
+    id: 'tradermade-live',
+    url: 'https://marketdata.tradermade.com/api/v1/live?currency={pairs}&api_key={access_key}',
   },
   {
-    id: 'tradermade-convert', url: 'https://marketdata.tradermade.com/api/v1/convert?from={from}&to={to}&amount={amount}&api_key={access_key}'
-  }
+    id: 'tradermade-convert',
+    url: 'https://marketdata.tradermade.com/api/v1/convert?from={from}&to={to}&amount={amount}&api_key={access_key}',
+  },
 ] as const satisfies readonly CurrencyProvider[]
 
 export default currencyProviders
