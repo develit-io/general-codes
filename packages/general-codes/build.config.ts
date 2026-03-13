8b50ea947f5288cacfa7b0b5d2407c4c6f6b7962
Kotkoroid <git@kotkoroid.com>
2026-03-13 12:59:10 +0100

    ci: fix build

--- packages/general-codes/build.config.ts
+++ packages/general-codes/build.config.ts
@@ -1,9 +1,16 @@
+import { fileURLToPath } from 'node:url'
+import { dirname, resolve } from 'node:path'
 import { defineBuildConfig } from 'unbuild'
+
+const __dirname = dirname(fileURLToPath(import.meta.url))
 
 export default defineBuildConfig({
   entries: ['./src/index'],
   outDir: 'dist',
   declaration: true,
+  alias: {
+    '@packages/general-codes': resolve(__dirname),
+  },
   hooks: {
     'rollup:options': (_ctx, options) => {
       // Override external to not externalize our internal paths
