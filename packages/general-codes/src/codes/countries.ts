a3f63b0ef047d83c0e126ec5b1e2e06bb11972f9
kubandam <m.kubanda1@gmail.com>
2026-05-26 11:35:25 +0200

    feat(countries): add Hong Kong to the countries list
    Signed-off-by: kubandam <m.kubanda1@gmail.com>

--- packages/general-codes/src/codes/countries.ts
+++ packages/general-codes/src/codes/countries.ts
@@ -729,6 +729,15 @@ const countries = [
     vat: 10,
     sanctions: true,
   },
+	{
+		code2: "HK",
+		code3: "HKG",
+		currency: "HKD",
+		phoneCode: "+852",
+		name: "Hong Kong",
+		native: "香港",
+		vat: 0,
+	},
   {
     code2: 'HN',
     code3: 'HND',
